4948f92d72b5cc71a092b9970e2dc7a7ca45105e
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2025-03-20 09:33:48 +0200

    Class work

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+function manchester(){
+    return "Manchester is back"
+}
+
+
+
+function area(p1, p2) {
+    p1 = 5;
+    p2 = 3;
+    return p1 * p2;
+  }
+
+  console.log(manchester());
+  console.log(area());
